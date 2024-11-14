a6c4292f2acab2308609310f511e321c94643537
Vijayagopikha <vijayagopikha@gmail.com>
2024-11-14 21:43:37 +0530

    push

--- library_management/library_management/settings.py
+++ library_management/library_management/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT':'3306',
         'USER':'root',
-        'PASSWORD':'EnakuTheriyum@123' #vanjula
+        'PASSWORD':'mysql@04password' #gops
     }
 }
 
