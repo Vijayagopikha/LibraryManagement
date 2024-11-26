58c894272575627437b03a684b804b50515dca6e
Vanjula <vanjulasankar@gmail.com>
2024-11-26 10:41:38 +0530

    homepage

--- library_management/library_management/settings.py
+++ library_management/library_management/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT':'3306',
         'USER':'root',
-        'PASSWORD':'Varsh@04' #gops
+        'PASSWORD':'EnakuTheriyum@123' #gops
     }
 }
 
